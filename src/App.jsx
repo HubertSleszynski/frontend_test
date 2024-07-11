b08f18a94a8df746eb59675041ffac19aaa82de0
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-07-11 13:48:32 +0200

    added header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   return (
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,20 @@
+import "../styles/Header.scss";
+import { FaHtml5 } from "react-icons/fa";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="main-header">
+      <a href="/" className="main-header__logo-link">
+        <FaHtml5 className="main-header__icon" />
+      </a>
+      <div className="main-header__details">
+        <h2 className="main-header__title">
+          Zadanie{" "}
+          <span className="main-header__title-highlight">rekrutacyjne</span>
+        </h2>
+      </div>
+    </header>
+  );
 };
+
 export default Header;
